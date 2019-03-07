500036a4842f4b6a76577553209376089a8caca7
baihuayang <1053091115@qq.com>
2019-03-07 17:49:34 +0800

    初始化导入

---
+++ jeeadmin/jeeadmin-web/src/main/java/com/jeeadmin/JeeAdminApplication.java
@@ -0,0 +1,22 @@
+package com.jeeadmin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@MapperScan("com.jeeadmin.mapper")
+public class JeeAdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeAdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JeeAdminApplication.class);
+    }
+}
---
+++ jeerigger/jeerigger-service/src/main/java/com/jeerigger/module/sys/mapper/SysUserPostMapper.java
@@ -0,0 +1,16 @@
+package com.jeerigger.module.sys.mapper;
+
+import com.jeerigger.frame.base.mapper.BaseMapper;
+import com.jeerigger.module.sys.entity.SysUserPost;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wangcy
+ * @since 2019-01-22
+ */
+public interface SysUserPostMapper extends BaseMapper<SysUserPost> {
+
+}
